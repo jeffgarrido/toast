d8524b479c30e4f64f3f7fbbd5a8b943f186f6ef
iron <ironmaturan7@gmail.com>
2017-02-02 00:49:12 +0800

    -iron

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Organization;
+use App\AuditLog;
 use Illuminate\Http\Request;
 
 class OrganizationController extends Controller
@@ -14,5 +15,34 @@ class OrganizationController extends Controller
 
     public function getOrgDetails(Organization $organization){
         return view('organizationpages.orgdetails', compact('organization'));
+    }
+
+    public function addOrg(Request $request){
+        $org = new Organization();
+
+        $org->Organization_Name = $request->input('Organization_Name');
+        $org->Description = $request->input('Description');
+        $org->Adviser_Id = $request->input('Adviser_Id');
+        $org->save();
+
+        //Create Audit Log
+        $this->createLog(
+            'Add Org',
+            'Org Name: '.$request->input('Code').                                           '\n'.
+            'Description: '.$request->input('Title').                                         '\n'.
+            'Adviser ID: '.$request->input('Description', 'No Description Provided').  '\n'
+        );
+
+        return back();
+    }
+
+    private function createLog($action, $description = ""){
+        $log = new AuditLog();
+
+        $log->AccountID = 1;
+        $log->Action = $action;
+        $log->Description = $description;
+
+        $log->save();
     }
 }
