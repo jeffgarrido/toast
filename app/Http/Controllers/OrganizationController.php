89abbb3e49e18d1d7d70f07c2d07d102d0115ec8
jeffgarrido <jepgarrido@gmail.com>
2017-02-06 11:52:29 +0800

    fix 2

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -22,7 +22,7 @@ class OrganizationController extends Controller
     }
 
     public function getAttendanceList(Event $event) {
-        $students = $event->students()->wherePivot('Attendance', 'not', 'null')->get();
+        $students = $event->students()->wherePivot('Attendance', '!=', 'null')->get();
         return view('organizationpages.attendancelist', compact('event', 'students'));
     }
 
