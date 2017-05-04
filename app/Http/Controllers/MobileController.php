eb42f059a9a5b4d6285fd249ff63c9254abb9fc4
jeffgarrido <jepgarrido@gmail.com>
2017-05-04 21:30:50 +0800

    Update

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -66,6 +66,81 @@ class MobileController extends Controller
     public function fetchStudent(Request $request) {
         $student = Student::where('StudentNumber', '=', $request->input('token'))->get()->load('studentOutcomes');
 
+        $student->load(array(
+            'events' => function ($query) {
+                $query->where('event_student.Attendance', '<>', '');
+            }
+        ));
+
+        foreach ($student->studentOutcomes()->with('performanceIndicators')->get() as $outcome) {
+            $outcome->pivot->Evaluation = 0;
+            $outcome->pivot->P1 = 0;
+            $outcome->pivot->P2 = 0;
+            $outcome->pivot->P3 = 0;
+            $outcome->pivot->EventEval = 0;
+            $p1ctr = 0;
+            $p2ctr = 0;
+            $p3ctr = 0;
+            $eventCtr = 0;
+
+            foreach ($student->SOEvaluations()->get() as $soEval) {
+                if($soEval->pivot->Evaluation == 0){
+                    continue;
+                }
+                $index = $outcome->performanceIndicators->search($soEval->performanceIndicator);
+                if($index === false){
+                    continue;
+                } else {
+                    switch ($index) {
+                        case 0:
+                            $outcome->pivot->P1 += $soEval->pivot->Evaluation;
+                            $p1ctr++;
+                            break;
+                        case 1:
+                            $outcome->pivot->P2 += $soEval->pivot->Evaluation;
+                            $p2ctr++;
+                            break;
+                        case 2:
+                            $outcome->pivot->P3 += $soEval->pivot->Evaluation;
+                            $p3ctr++;
+                            break;
+                    }
+                }
+            }
+
+            $outcome->pivot->P1 = round($outcome->pivot->P1 / (($p1ctr == 0) ? 1 : $p1ctr), 2);
+            $outcome->pivot->P2 = round($outcome->pivot->P2 / (($p2ctr == 0) ? 1 : $p2ctr), 2);
+            $outcome->pivot->P3 = round($outcome->pivot->P3 / (($p3ctr == 0) ? 1 : $p3ctr), 2);
+
+            foreach ($student->events()->where('event_student.Attendance', '<>', '')->get() as $studentEvent) {
+                if ($studentEvent->studentOutcomes()->get()->contains($outcome)) {
+                    $eventCtr++;
+                }
+            }
+
+            $eventScore = $eventCtr / (($outcome->Events_Minimum == 0) ? 1 : $outcome->Events_Minimum) * 100;
+
+            if($eventCtr <= 0){
+                $outcome->pivot->EventEval = 0;
+            }elseif($eventScore < 40) {
+                $outcome->pivot->EventEval = 1;
+            } elseif ($eventScore < 60) {
+                $outcome->pivot->EventEval = 2;
+            }elseif ($eventScore < 80) {
+                $outcome->pivot->EventEval = 3;
+            }else {
+                $outcome->pivot->EventEval = 4;
+            }
+
+            $outcome->pivot->Evaluation = round(
+                (($outcome->pivot->P1 * $outcome->performanceIndicators[0]->Weight / 100) +
+                    ($outcome->pivot->P2 * $outcome->performanceIndicators[1]->Weight / 100) +
+                    ($outcome->pivot->P3 * $outcome->performanceIndicators[2]->Weight / 100) +
+                    ($outcome->pivot->EventEval * $outcome->Event_Weight / 100)), 2);
+
+            $outcome->pivot->update();
+        }
+
         return $student->toJson();
     }
 
